e275845a4893ac7709dcec774b994b8b113539e5
karlito786 <119797568+karlito786@users.noreply.github.com>
2023-10-26 12:03:04 +0200

    Add files via upload

--- IntegerModifiers/main.cpp
+++ IntegerModifiers/main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 
 int main(){
-  auto result = (10 <=> 20) > 0;
-  std::cout << result << std::endl;
+  signed int value1 {10};
+  signed int value2 {-300};
+
+  std::cout << "value1 : " << value1 << std::endl;
+  std::cout << "value2 : " << value2 << std::endl;
+  std::cout << "sizeof(value1) : " << sizeof(value1) << std::endl;
+  std::cout << "sizeof(value2) : " << sizeof(value2) << std::endl;
+
+  unsigned int value3 {4};
+  // unsigned int value4 {-5}; // Compiler error
+ 
+  return 0;
 }
